10e7d466fd646d46e050fa46ca4992982e13aca8
Charph <chaofengdang@gmail.com>
2021-07-16 17:30:04 +0800

    update

--- mongoose/db.js
+++ mongoose/db.js
@@ -6,23 +6,23 @@
  * @LastEditTime: 2021-01-11 16:39:08
  */
 
-const mongoose = require('mongoose');
-const dbhost = 'mongodb://localhost:27017/';
-const dbname = 'DATABASE_TEST';
+const mongoose = require('mongoose')
+const dbhost = 'mongodb://localhost:27017/'
+const dbname = 'DATABASE_TEST'
 
 module.exports = {
-    connect() {
-        mongoose.connect(dbhost + dbname, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
+  connect() {
+    mongoose.connect(dbhost + dbname, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
 
-        let db = mongoose.connection;
-        db.on('error', () => {
-            console.error('connection error:');
-        });
-        db.once('open', callback => {
-            console.log('connect success!', callback);
-        });
-    }
-};
+    let db = mongoose.connection
+    db.on('error', () => {
+      console.error('connection error:')
+    })
+    db.once('open', callback => {
+      console.log('connect success!', callback)
+    })
+  }
+}
